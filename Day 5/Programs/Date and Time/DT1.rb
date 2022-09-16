e9d6f898e5922a7a57612a089099b2e9e8c5ef48
Ravindra <ravindrakaliya00723@gmail.com>
2022-09-16 18:24:30 +0530

    Commited

---
+++ Day 5/Programs/Date and Time/DT1.rb
@@ -0,0 +1,20 @@
+# using date and time
+
+t = Time.new
+
+# Inspect Current Time
+puts "current Time : "+ t.inspect
+
+puts "Year : #{t.year}"
+puts "Month : #{t.month}"
+puts "Day : #{t.day}"
+puts "Week Day : #{t.wday}"
+puts "Year's Day : #{t.yday}"
+puts "Hour : #{t.hour}"
+puts "Minute : #{t.min}"
+puts "Second : #{t.sec}"
+puts "Micro-Second : #{t.usec}"
+puts "Time Zone : #{t.zone}"
+
+
+
